bf8017865eefc8fbe81b1d8631ddfe478e735883
Morre95 <erik.moren78@gmail.com>
2023-06-05 12:25:11 +0200

    Drawing a grid

--- ConsoleSnakeCompetition/Program.cs
+++ ConsoleSnakeCompetition/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace ConsoleSnakeCompetition;
+﻿using System.Drawing;
+
+namespace ConsoleSnakeCompetition;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int rows = 20, cols = 80;
+        string toPrint = "";
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                if (row == 0 || row > rows - 2 || col == 0 || col > cols - 2)
+                {
+                    toPrint += "*";
+                }
+                else
+                {
+                    toPrint += " ";
+                }
+            }
+            toPrint += "\n";
+        }
+
+        Console.WriteLine(toPrint);
     }
+
+    
 }
